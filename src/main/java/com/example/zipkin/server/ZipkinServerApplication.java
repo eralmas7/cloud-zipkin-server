83b95c3d1c88fc31edc0afa0566a01c6a417f99a
eralmas7 <eralmas7@yahoo.com>
2022-01-01 22:59:11 +0530

    Upgraded spring boot version to 2021.0.0

--- src/main/java/com/example/zipkin/server/ZipkinServerApplication.java
+++ src/main/java/com/example/zipkin/server/ZipkinServerApplication.java
@@ -2,10 +2,13 @@ package com.example.zipkin.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import zipkin.server.EnableZipkinServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import zipkin2.server.internal.EnableZipkinServer;
 
 @SpringBootApplication
 @EnableZipkinServer
+@EnableEurekaClient
 public class ZipkinServerApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/example/zipkin/server/ZipkinServerApplicationTest.java
+++ src/test/java/com/example/zipkin/server/ZipkinServerApplicationTest.java
@@ -1,13 +1,9 @@
 package com.example.zipkin.server;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
 import com.example.zipkin.server.ZipkinServerApplication;
 
-@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = ZipkinServerApplication.class)
 public class ZipkinServerApplicationTest {
 
