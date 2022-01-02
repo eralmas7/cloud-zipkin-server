5f8261af19b4f3b9fa2d7dd6b8c2cec34d5a8175
eralmas7 <eralmas7@yahoo.com>
2022-01-02 23:12:16 +0530

    Removed debug logging.

--- src/test/java/com/example/zipkin/server/ZipkinServerApplicationTest.java
+++ src/test/java/com/example/zipkin/server/ZipkinServerApplicationTest.java
@@ -2,7 +2,6 @@ package com.example.zipkin.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import com.example.zipkin.server.ZipkinServerApplication;
 
 @SpringBootTest(classes = ZipkinServerApplication.class)
 public class ZipkinServerApplicationTest {
